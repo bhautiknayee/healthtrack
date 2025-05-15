280391dab99d1c587d5b34c046fc43fdbe186df6
bhautiknayee <nayeebhautik@gmail.com>
2025-05-15 15:14:10 -0400

    Added patient-service microservice

---
+++ patient-servic/src/main/java/com/healthtrack/patient_servic/PatientServicApplication.java
@@ -0,0 +1,13 @@
+package com.healthtrack.patient_servic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientServicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientServicApplication.class, args);
+	}
+
+}
---
+++ patient-servic/src/test/java/com/healthtrack/patient_servic/PatientServicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthtrack.patient_servic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
